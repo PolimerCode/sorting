2d0ae414079347c0375bf49e2e12930b639c3969
PoliSours <kgprrrr@gmail.com>
2025-10-18 02:08:24 +0300

    first file
    just messing things around a lil bit

---
+++ sorting.py
@@ -0,0 +1,8 @@
+numbers = [1, 5, 6, 2, 7, 8, 3]
+sort_numbers = sorted(numbers)
+
+words = ["lelel", "abongus", "cat", "dog"]
+wsorted = sorted(words, key=lambda x: x[-1])
+
+print(wsorted)
+print(sort_numbers)
