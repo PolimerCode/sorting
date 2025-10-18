2018b308cf2b3f3504a32ee3c2452d3288ca3d1e
PoliSours <kgprrrr@gmail.com>
2025-10-18 10:59:32 +0300

    advanced soring (for me)

--- learn/sorting.py
+++ learn/sorting.py
@@ -1,11 +1,13 @@
 numbers = [1, 5, 6, 2, 7, 8, 3]
 sort_numbers = sorted(numbers)
 
-words = ["lelel", "abongus", "cat", "dog"]
-wsorted = sorted(words, key=lambda x: x[-1])
+words = ["cat", "obama", "dog", "obamb", "obamc"]
+# wsorted = sorted(words, key=lambda x: x[-1]) # сортировка по букве с конца
+wsorted = sorted(words, key=lambda x: (len(x), x)) # сорт по длинне, а затем по алфавиту
 
-print(wsorted)
-print(sort_numbers)
+# print(wsorted)
+# print(sort_numbers)
 
-#test for git inside vs code nvm
-#tst2 nvm
+cities = ["Берлин", "Париж", "Лондон", "Рим", "Амстердам", "Осло"]
+sort_cities = sorted(cities, key=lambda x: (len(x), x[-1])) # сорт названий городов по длинне и по последней букве если длина одинакова
+print(sort_cities)
